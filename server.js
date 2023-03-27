29a772caf4cd3f88492079cf86c7c30a7d17c9d7
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-03-27 15:06:52 -0400

    Mongoose Connection

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+/** 
+ * ---- MONGOOSE CONNECTION ----
+ */
+mongoose.set('strictQuery', false); 
+const mongoDBURL = process.env.db_url;
+const mongoDBOptions = { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+} 
+mongoose.connect(mongoDBURL, mongoDBOptions)
+  .catch((err) => console.log(`Error Connecting: ${err}`))
+/**
+ * -----------------------------
+ */
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +36,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
