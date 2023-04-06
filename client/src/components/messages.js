4615217dc2ce71ef1824dd62733ef601ea3c28f8
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-04-06 10:35:42 -0400

    Basic messages layout with responsive search bar using fake data

--- client/src/components/messages.js
+++ client/src/components/messages.js
@@ -3,16 +3,38 @@ import { useCookies } from "react-cookie";
 import { useNavigate, useParams } from "react-router-dom";
 import Loading from "./loading";
 import Navbar from "./navbar";
+import '../styles/messages.css';
 
 const Messages = (props) => {
 
     const [cookie, setCookie] = useCookies(['token']);
     const [auth, setAuth] = useState(null);
     const [display, setDisplay] = useState();
+    const [messagesDisplay, setMessagesDisplay] = useState();
+    const [searchQuery, setSearchQuery] = useState("");
     const userId = useRef();
-    const currentUser = useRef();
+    const [currentUser, setCurrentUser] = useState();
     const [allUsers, setAllUsers] = useState();
     const navigate = useNavigate();
+
+    const Data = [
+        {
+            id: 1,
+            text: "Dog"
+        },
+        {
+            id: 2,
+            text: "Cat"
+        },
+        {
+            id: 3,
+            text: "Bear"
+        },
+        {
+            id: 4,
+            text: "Tiger"
+        },
+    ]
 
     // Anytime the cookie changes, set auth
     useEffect(() => {
@@ -22,8 +44,8 @@ const Messages = (props) => {
             if(cookie.token) {
                 const checkTokenResponse = await props.checkToken(`${props.serverURL}/messages`, cookie.token);
                 userId.current = checkTokenResponse.userToken.sub;
-                currentUser.current = checkTokenResponse.currentUser;
                 setAllUsers(checkTokenResponse.allUsers);
+                setCurrentUser(checkTokenResponse.currentUser);
                 setAuth(checkTokenResponse.auth)
             }
             else setAuth(false);
@@ -44,26 +66,84 @@ const Messages = (props) => {
         }
     }, [auth])
 
-    // Anytime all users changes set the display
+    // Anytime current user changes set the display
     useEffect(() => {
 
         // If the user is authorized and all users exist render page
-        if (auth === true && allUsers) {
-            console.log(allUsers)
-            
+        if (auth && currentUser) {
+
             setDisplay(
                 <div>
-                    <Navbar currentUser={currentUser.current} serverURL={props.serverURL} />
-                    <h1>Messages</h1>
+                    <div className="sidebar-currentChats">
+                        <h1>Chats</h1>
+                        <p>John Doe</p>
+                        <p>Bob the Builder</p>
+                        <p>etc..</p>
+                    </div>
                 </div>
             )
         }
 
+    }, [currentUser])
+
+    // Anytime all users changes set messages display
+    useEffect(() => {
+        if(auth && allUsers) {
+            console.log(allUsers)
+
+            setMessagesDisplay(
+                <div className="chat-container">
+
+                    <div className="chat-message">
+                        <p>Message 1</p>
+                    </div>
+                    <div className="chat-message">
+                        <p>Message 2</p>
+                    </div>
+                </div>
+            )
+        }
     }, [allUsers])
 
     return (
         <div className="Page">
-            {display}
+            {(currentUser)
+                ? <Navbar currentUser={currentUser} serverURL={props.serverURL} />
+                : <></>
+            }
+            <div className="messages-container">
+                <div className="messages-sidebar">
+                    {display}
+                </div>
+                <div className="messages-main">
+                    
+                    <div className="messages-searchbar">
+                        <label>
+                            To:
+                            <input type="text" placeholder="Search A Name" onChange={e => setSearchQuery(e.target.value)}/>
+                        </label>
+                        <div className="messages-searchbar-suggestions">
+                            {
+                                Data.filter(post => {
+                                    if (searchQuery === '') {
+                                        return post;
+                                    } else if (post.text.toLowerCase().includes(searchQuery.toLowerCase())) {
+                                        return post;
+                                    }
+                                }).map((post, index) => (
+                                    <div key={index}>
+                                        <p>{post.text}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    {messagesDisplay}
+                </div>
+                
+            </div>
+
         </div>
     )
 }
