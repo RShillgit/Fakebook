1c00701bb51f6a7f40dbf0cc6f64bc25dd5d7e2b
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-04-15 16:22:28 -0400

    Added different setheader middleware

--- server.js
+++ server.js
@@ -74,6 +74,22 @@ app.use(function (req, res, next) {
 });
 */
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+
 app.use('/', indexRouter);
 app.use('/posts', passport.authenticate('jwt', {session: false}), postsRouter);
 app.use('/profile', passport.authenticate('jwt', {session: false}), profileRouter);
